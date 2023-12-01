4c53741a17dabc8fb6a71ecd62020fd66f43bc38
moabukar <mo-abukar10@hotmail.com>
2023-12-01 18:37:27 +0000

    solve using simple method

--- go/leetcode/w1-sequences/prodarrayexceptself.go
+++ go/leetcode/w1-sequences/prodarrayexceptself.go
@@ -17,23 +17,46 @@ import "fmt"
 // Input: nums = [-1,1,0,-3,3]
 // Output: [0,0,9,0,0]
 
+// Method 1
+// func productExceptSelf(nums []int) []int {
+// 	// create a slice answer with the same length as nums
+// 	answer := make([]int, len(nums))
+
+// 	//
+// 	left := 1
+// 	right := 1
+
+// 	for i := 1; i < len(nums); i++ {
+// 		if i <= (len(nums)-1)/2 {
+// 			answer[i] = 1
+// 			answer[len(nums)-i-1] = 1
+// 		}
+// 		answer[len(nums)-i-1] *= right
+// 		answer[i] *= left
+// 		left *= nums[i]
+// 		right *= nums[len(nums)-i-1]
+// 	}
+// 	return answer
+// }
+
+// Method 2
+
 func productExceptSelf(nums []int) []int {
-	// create a slice answer with the same length as nums
 	answer := make([]int, len(nums))
 
-	//
-	left := 1
-	right := 1
+	// first element of answer is set to 1.
+	// As there are no elements to the left of the first element in nums so the product of elements to its left is 1
+	answer[0] = 1
+	// loop starts from 2nd element - and loops through array
+	for i := 1; i < len(nums); i++ {
+		answer[i] = answer[i-1] * nums[i-1]
+	}
 
-	for i := 0; i < len(nums); i++ {
-		if i <= (len(nums)-1)/2 {
-			answer[i] = 1
-			answer[len(nums)-i-1] = 1
-		}
-		answer[len(nums)-i-1] *= right
-		answer[i] *= left
-		left *= nums[i]
-		right *= nums[len(nums)-i-1]
+	rightProduct := 1
+	// loop starts from last element - and loops through array
+	for i := len(nums) - 1; i >= 0; i-- {
+		answer[i] *= rightProduct
+		rightProduct *= nums[i]
 	}
 	return answer
 }
