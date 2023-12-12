e71bf74fc66d83a23119f3d9fbc321a73479912c
moabukar <mo-abukar10@hotmail.com>
2023-12-12 23:50:55 +0000

    day 12

--- aoc-2023/day12/day12.go
+++ aoc-2023/day12/day12.go
@@ -135,12 +135,11 @@ func Part2(input string) int {
 }
 
 func main() {
-	fmt.Println("--2023 day 11 solution--")
 	start := time.Now()
-	fmt.Println("part1: ", Part1(inputDay))
+	fmt.Println("Part 1: ", Part1(inputDay))
 	fmt.Println(time.Since(start))
 
 	start = time.Now()
-	fmt.Println("part2: ", Part2(inputDay))
+	fmt.Println("Part 2: ", Part2(inputDay))
 	fmt.Println(time.Since(start))
 }
