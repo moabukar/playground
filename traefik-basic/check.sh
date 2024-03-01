49af9b5780f76eca9ab790d8777c1ab65aaebcf8
moabukar <mo-abukar10@hotmail.com>
2024-03-01 16:25:46 +0000

    add a little check script

---
+++ traefik-basic/check.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "Requesting next container"
+curl localhost:80
+echo ""
+sleep 0.5
+
+echo "Requesting next container"
+curl localhost:80
+echo ""
+sleep 0.5
+
+echo "Requesting next container"
+curl localhost:80
+echo ""
+sleep 0.5
+
+echo "Requesting next container"
+curl localhost:80
+echo ""
+sleep 0.5
