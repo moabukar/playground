bb661165ca8b8ff412707eee90e0c9fb4f83ba74
moabukar <mo-abukar10@hotmail.com>
2024-04-07 20:38:45 +0100

    add github runner k8s setup

--- k8s-gh-runner-simple/entrypoint.sh
+++ k8s-gh-runner-simple/entrypoint.sh
@@ -3,7 +3,7 @@
 # ACTIONS_RUNNER_INPUT_NAME is used by config.sh
 ACTIONS_RUNNER_INPUT_NAME=$HOSTNAME
 # get regsistration token for this runnner
-ACTIONS_RUNNER_INPUT_TOKEN="$(curl -sS --request POST --url "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/actions/runners/registration-token" --header "authorization: Bearer ${GITHUB_TOKEN}"  --header 'content-type: application/json' | jq -r .token)"
+ACTIONS_RUNNER_INPUT_TOKEN="$(curl -sS --request POST --url "https://api.github.com/repos/moabukar/playground/actions/runners/registration-token" --header "authorization: Bearer ${GITHUB_TOKEN}"  --header 'content-type: application/json' | jq -r .token)"
 # configure runner
 export RUNNER_ALLOW_RUNASROOT=1
 /runner/config.sh --unattended --replace --work "/tmp" --url "$ACTIONS_RUNNER_INPUT_URL" --token "$ACTIONS_RUNNER_INPUT_TOKEN" --labels k8s-github-runner
