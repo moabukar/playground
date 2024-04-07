7febc643451e1881e98675a3ed31eb9b19d3d4ae
moabukar <mo-abukar10@hotmail.com>
2024-04-07 21:21:39 +0100

    add var for token

---
+++ gh-runner-basic/README.md
@@ -0,0 +1,6 @@
+# Setup
+
+```sh
+docker build -t ghr .
+docker run -e GITHUB_RUNNER_TOKEN=<TOKEN_HERE> ghr
+```
--- gh-runner-basic/entrypoint.sh
+++ gh-runner-basic/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
+TOKEN=${GITHUB_RUNNER_TOKEN}
 # Configure the runner
-./config.sh --url https://github.com/moabukar/playground --token <TOKEN_HERE>
+./config.sh --url https://github.com/moabukar/playground --token ${TOKEN}
 
 # Run the runner
 ./run.sh
