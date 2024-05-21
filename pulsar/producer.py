5bd040a34021d2db0c6eb4ef4f13ea188c6d34b9
moabukar <mo-abukar10@hotmail.com>
2024-05-21 20:41:10 +0100

    play around with pulsar

--- pulsar/producer.py
+++ pulsar/producer.py
@@ -4,6 +4,8 @@ client = pulsar.Client('pulsar://localhost:6650')
 producer = client.create_producer('my-topic')
 
 for i in range(10):
-    producer.send(('test-pulsar-%d' % i).encode('utf-8'))
+    producer.send(('mo-test-message-%d' % i).encode('utf-8'))
+
+# producer.send('Test')
 
 client.close()
